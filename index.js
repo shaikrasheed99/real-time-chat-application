0b07291df88c3a48307414dd3305f53a30e1ddcc
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-08 11:50:00 +0530

    Add. Logic to add chat to the ui

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const defaultUsername = "Anonymous";
 const chatroom = new Chatroom(defaultRoom, defaultUsername);
 const chatUi = new ChatUI(chatList);
 
+addChatForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const message = addChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => {
+            addChatForm.reset();
+        })
+        .catch((error) => {
+            console.log("Error is: ", error);
+        });
+});
+
 chatroom.getChats((data) => {
     chatUi.render(data);
 })
