7595ed2bb101a8535dc5bc6bc3f7689111c158d2
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-08 11:38:17 +0530

    Add. Logic to render ui with chats

---
+++ chatui.js
@@ -0,0 +1,22 @@
+class ChatUI {
+    constructor(list) {
+        this.list = list;
+    }
+
+    render = (data) => {
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            { addSuffix: true }
+        );
+
+        const messageTemplate = `
+            <li class="list-group-item">
+                <span class="username">${data.username}</span>: 
+                <span class="message">${data.message}</span>
+                <div class="time">${when}</div>
+            </li>
+        `;
+
+        this.list.innerHTML += messageTemplate;
+    };
+}
